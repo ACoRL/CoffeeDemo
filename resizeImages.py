62b4d922e120e0b9aec7217041c105b609f2a620
fabeliflow <fabeliflow@gmail.com>
2016-04-25 16:50:59 -0400

    documentation and reformatting done

--- resizeImages.py
+++ resizeImages.py
@@ -5,22 +5,33 @@ _dir = "/home/acorl/Desktop/coffee_demo/rgb_frames/imgs"
 
 size = 300,230
 
-hello = "HELLOOOOOO"
+'''
+All this program does is take all the images from
+the coffee_demo/rgb_frame/imgs directory,
+resize all of them using whatever method we choose
+and applying whatever operations we want as well,
+then to sort all of them into the correct
+state and direction folder.
+'''
 
 for file in os.listdir(_dir):
         print(file, end='    :    ')
         print(file.split("_")[0])
-        #img = cv2.imread(_dir + "/" + file,0)
-        im = Image.open(_dir + "/" + file)
-        im.thumbnail(size, Image.ANTIALIAS)
-        im.save(_dir + "/../" + file.split("_")[0] + "/" + file.split("_")[-1][0] + "/" + file, "PNG")
-        #cv2.imshow("Show", img)
-        #img_dst = img
+        # Image resize using PIL
+        #im = Image.open(_dir + "/" + file)
+        #im.thumbnail(size, Image.ANTIALIAS)
+        #im.save(_dir + "/../" + file.split("_")[0] + "/" + file.split("_")[-1][0] + "/" + file, "PNG")         #cv2.imshow("Show", img)
         
-        #img = cv2.pyrDown(img, (.001,.001))
+        # Image resize using OpenCV
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img = cv2.resize(img, size)
         
-        #cv2.imwrite(_dir + "/../" + file.split("_")[0] + "/" + file.split("_")[-1][0] + "/" + file,img)
- 
+        img = cv2.imread(_dir + "/" + file,0)
+        img_dst = img
         
+        img = cv2.pyrDown(img, (.001,.001))
         
-
+        # Save the image in the directory containing the imgs folder in a
+        # folder named after the state and a folder named after the  direction
+        # Both of which are specified in the file name of the image opened.
+        cv2.imwrite(_dir + "/../" + file.split("_")[0] + "/" + file.split("_")[-1][0] + "/" + file,img)
--- robotLogicContingency_v2.py
+++ robotLogicContingency_v2.py
@@ -1,43 +1,40 @@
 import cv2
 import freenect
-import numpy as np
 import skimage.measure
-import sys
-import numpy
-import os
 import serial
 import time
-from PIL import Image
+#from PIL import Image
 from skimage import color
 from LBAR_Class import LBAR
 
+# Image resizing size
 size = 300,230
 
-print(sys.argv)
+conf = float(sys.argv[-1])
 
 DEBUGGING = True
 
-print("Connecting")
+# Connect to bluetooth
+if DEBUGGING:
+    print("Connecting")
 lbar = LBAR("/dev/rfcomm0", 9600)
-print("Created object")
+if DEBUGGING:
+    print("Created object")
 time.sleep(3)
-print("Connected")
+if DEBUGGING:
+    print("Connected")
 
 print("Done now, GOOOOO")
 
 
 static_frame,_ = freenect.sync_get_video()
 
-#scale down current frame
-#static_frame = cv2.pyrDown(static_frame, (50,50))
-
-
 # Direction Constants
 STRAIGHT = 0
 LEFT     = 7
 RIGHT    = 1
 
-DIRECTIONS = (STRAIGHT, LEFT, RIGHT)
+DIRECTIONS = [STRAIGHT, LEFT, RIGHT]
 
 TURN_INDICATOR = {1 :LEFT,  3 :LEFT,  5 :RIGHT,
                   7 :RIGHT, 9 :RIGHT, 11:LEFT,
@@ -58,41 +55,29 @@ DIRECTION_IMAGES = [
 
 def driveForward():
     lbar.send(1)
-    
-    
 
 def driveLeft():
     lbar.send(7)
     time.sleep(.5)
-    
-    
 
 def driveRight():
     lbar.send(9)
     time.sleep(.5)
-    
-    
-    
+
 def turnLeft():
     lbar.send(5)
     time.sleep(.5)
     
-    
-    
 def turnRight():
     lbar.send(6)
     time.sleep(.5)
-    
-    
 
 def keywithmaxval(d): # Stack Overflow : the wolf
-     """ a) create a list of the dict's keys and values; 
-         b) return the key with the max value"""  
+     """ a) Create a list of the dict's keys and values; 
+         b) Return the key with the max value"""  
      v=list(d.values())
      k=list(d.keys())
      return k[v.index(max(v))]
-     
-
 
 def compare_imgs(curr_frame, end_frame):
     '''
@@ -101,95 +86,56 @@ def compare_imgs(curr_frame, end_frame):
         0 represents completely dissimilar,
         1 represents the same image
     '''
-    #curr_frame = calc_hist(curr_frame)
-    #end_frame = calc_hist(end_frame)
-    #h1 = np.histogramdd(curr_frame)
-    #h2 = np.histogramdd(end_frame)
-    #print(curr_frame)
-    
-    
     return skimage.measure.structural_similarity(curr_frame, end_frame)
-    #return cv2.compareHist(curr_frame, end_frame)
-    #val = mse(curr_frame, end_frame)
-    #d = cv2.compareHist(np_hist_to_cv(h1), np_hist_to_cv(h2),
-    #                cv2.cv.CV_COMP_BHATTACHARYYA)
-    #return d
-
 
 def init_images():
     '''
     Get all the images for a given state and direction 
     FROM THE something lol and return it inside an array.
     '''
-    
     for state in range(1):
-        for direction in ['0', '1', '7']:
+        for direction in map(str, DIRECTIONS):
             if DEBUGGING:
-                print("Loading images for state " + str(state) + " and direction " + direction)
-            _dir = "/home/acorl/Desktop/coffee_demo/rgb_frames/" + str(state) + "/" + str(direction) + "/"
-            if DEBUGGING:
-                count = 0
+                print("Loading images for state " + str(state)
+                      + " and direction " + direction)
+            _dir = "/home/acorl/Desktop/coffee_demo/rgb_frames/"
+                   + str(state) + "/" + str(direction) + "/"
+            
+            # Loading grayscale images in the correct state and direction
             for filename in os.listdir(_dir):
-                if DEBUGGING:
-                    count += 1
                 DIRECTION_IMAGES[state][direction].append(color.rgb2gray(cv2.imread(_dir + filename)))
-            if DEBUGGING:
-                print("Loaded " + str(count) + " images.")
-
-
-def mse(imageA, imageB):
-    # the 'Mean Squared Error' between the two images is the
-    # sum of the squared difference between the two images;
-    # NOTE: the two images must have the same dimension
-    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-    err /= float(imageA.shape[0] * imageA.shape[1])
-	
-    # return the MSE, the lower the error, the more "similar"
-    # the two images are
-    return err
-
-def np_hist_to_cv(np_histogram_output):
-    counts, bin_edges = np_histogram_output
-    return counts.ravel().astype('float32')
-
-def calc_hist(img):
-    return cv2.calcHist([img],[0],None,[256],[0,256])
 
 def get_direction_images(state, direction):
     return DIRECTION_IMAGES[state][str(direction)]
 
-
 def get_final_state_image(state, direction=0):
     '''
-    Get the last image for the given state
-    or get the first image for the next state
+    Get the last image for the given state by
+    getting the last image in the array
+    (This assumes the images are ordered)
     '''
     return get_direction_images(state, direction)[-1]
 
-
-
-def transition_reached(curr_frame, final_state_image, confidence_threshold=.7):
-    '''Check for state changes'''
+def similar_enough(curr_frame, final_state_image, confidence_threshold=.7):
+    '''Returns True if the curr_frame is similar enough to final_state_image, otherwise...'''
     conf = compare_imgs(curr_frame, final_state_image)
     return conf >= confidence_threshold
     
-
-
-
 def get_direction(curr_frame, state):
     '''Determine which direction the current frame is most likely pointing'''
     direc_possibilities = {0:0, 1:0, 7:0}
+    
     for direction in DIRECTIONS:
         if DEBUGGING:
             print("Checking images for direction:" + str(direction))
         img_directions = get_direction_images(state, direction)
         for img in img_directions:
-            if transition_reached(curr_frame, img):
+            if similar_enough(curr_frame, img):
                 direc_possibilities[direction] += 1
-    print(direc_possibilities)
+                
+    if DEBUGGING:
+        print(direc_possibilities)
     return keywithmaxval(direc_possibilities)
-
-
 
 def rotate(direction):
     '''Direction is an integer, value given by the constants'''
@@ -198,14 +144,11 @@ def rotate(direction):
     else:
         turnRight()
 
-
-
 def navigate(direction):
     '''
     Sends the commands to go straight or to correct the
     course in case the robot gets off-center.
     '''
-    #similar = compare_imgs(curr_frame, final_state_image)
     if DEBUGGING:
         print("Have NOT reached a state change")
         
@@ -222,44 +165,58 @@ def navigate(direction):
             print("Looking right, drive left")
         driveLeft()
     else:
-        #freak out mode engaged
+        # Freak out mode engaged
         if DEBUGGING:
             print("I don't know where to drive.")
         turnLeft()
         pass
 
-#from skimage.viewer import ImageViewer
 def main():
+    # The range where we allow before we consider 
+    # movement left or right.
+    # Note, look at a normal distribution, we're
+    # talking about allowing a range for how much
+    # movement we allowing before we we start
+    # to say whether there was movement to the
+    # left or to the right.
+    alpha = 0
+    
+    # Get previous image, resize it, convert it to grayscale.
     prevgray,_ = freenect.sync_get_video()
     prevgray = cv2.cvtColor(prevgray, cv2.COLOR_BGR2GRAY)
-    prevgray = cv2.resize(prevgray, (200, 200))
+    prevgray = cv2.resize(prevgray, size)
     
     while True:
+        # Get new image, resize it, convert it to grayscale.
         gray,_ = freenect.sync_get_video()
         gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
-        gray = cv2.resize(gray, (200, 200))
+        gray = cv2.resize(gray, size)
         
+        # Calculate the optical flow using this handy built in method.
         flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        
+        # Set the newly gathered image to the previous one.
         prevgray = gray
         
+        # Set the steps for do.
         step = 16
         
+        # Draw the dots and lines on the screen.
         h, w = gray.shape[:2]
         y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2, -1).astype(int)
         fx, fy = flow[y,x].T
         
+        # Get the average amount of displacement within the screen
+        # with positive being left and negative being right.
         fx_avg = np.average(fx)
         
-        if ((fx_avg > 0)):
-        
-            print('Moved left')
-            #driveRight()
-            
-        elif ((fx_avg < 0)):
-        
+        # Keep it straight!
+        if ((fx_avg > alpha)):
             print('Moved right')
             #driveLeft()
-            
+        elif ((fx_avg < -alpha)):
+            print('Moved left')
+            #driveRight()
         else:
             print('Looking straight')
             #driveForward()
